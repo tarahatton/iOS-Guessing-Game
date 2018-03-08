b1654f0113946f25bd418b6a5d2e936b32c53979
Shontara Hatton <shontarahatton@Interapts-MacBook-Pro-106.local>
2018-03-08 14:14:01 -0500

    iOS Guessing Game

---
+++ Guessing Game iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guessing Game iOS
+//
+//  Created by Shontara Hatton on 3/8/18.
+//  Copyright © 2018 Shontara Hatton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
